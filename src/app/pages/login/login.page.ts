4f5ae34d2d98c9a6907a323f6851b7da655bcefe
Igovi <igovi9910@gmail.com>
2022-02-18 10:07:20 -0300

    update: login page html and css completed

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
+  public password:string = '';
+  public userName:string = '';
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  addItemUser(event){
+    this.userName = event
+  }
+  addItemPass(event){
+    this.password = event
+  }
+  save(){
+    console.log(this.userName);
+    console.log(this.password);
+  }
 }
