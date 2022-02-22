02ce38e9bf822b6a6b58417493267c606e8b969b
Igovi <igovi9910@gmail.com>
2022-02-22 17:22:56 -0300

    update: Components module to declare and exports new components

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,3 +1,6 @@
+import { CardComponent } from './card/card.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { LoginInputComponent } from './login-input/login-input.component';
 import { NgModule } from '@angular/core';
@@ -12,11 +15,17 @@ import { TitleComponent } from './title/title.component';
     ],
     declarations: [
         TitleComponent,
-        LoginInputComponent
+        LoginInputComponent,
+        FooterComponent,
+        CarouselComponent,
+        CardComponent
     ],
     exports:[
         TitleComponent,
-        LoginInputComponent
+        LoginInputComponent,
+        FooterComponent,
+        CarouselComponent,
+        CardComponent
     ]
   })
   export class ComponentsModule {}
